de9121e69b35a51623f39f5bed3db056adf6e1d0
Michael <iceyarmu@gmail.com>
2025-05-26 01:37:57 +0800

    增加build编译脚本

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 $env:HTTPS_PROXY="http://127.0.0.1:10808"
 $env:HTTP_PROXY="http://127.0.0.1:10808"
 
+docker compose down
 docker build --build-arg PUPPETEER_SKIP_DOWNLOAD=0 --build-arg TARGETPLATFORM=linux/amd64 -t rsshub-with-chromium .
-docker save -o rsshub.tar rsshub-with-chromium
+docker compose up -d
+docker image prune -f
+#docker save -o rsshub.tar rsshub-with-chromium
